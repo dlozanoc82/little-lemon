9ce682a9166728a27a59f45dccdcec5a18ec214e
DCODER <dlozanoc862@gmail.com>
2024-09-10 09:34:44 -0500

    feat: add Footer component and its styles
    - Created the Footer component
    - Added CSS styles for the Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import logoMonocromatico from '../../assets/logo-mono.png'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+      <footer className="footer">
+      <div className="footer-container">
+        <img className="footer-logo" src={logoMonocromatico} alt="logo-footer"></img>
+        <div className="footer-site-map">
+          <p className="footer-title"> SITEMAP</p>
+          <Link className="footer-item" to="/">Home</Link>
+          <Link className="footer-item" to="/about">About</Link>
+          <Link className="footer-item" to="/menu">Menu</Link>
+          <Link className="footer-item" to="/reservations">Reservations</Link>
+          <Link className="footer-item" to="/order-online">Order Online</Link>
+          <Link className="footer-item" to="/login">Login</Link>
+        </div>
+        <div className="footer-contact">
+          <p className="footer-title"> CONTACT</p>
+          <p className="footer-item"> 123 Citrus Lane</p>
+          <p className="footer-item"> 123-456-7890</p>
+          <p className="footer-item"> little.lemon@lemon.com</p>
+        </div>
+        <div className="footer-social">
+          <p className="footer-title"> SOCIAL MEDIA LINKS</p>
+          <a className="footer-item" href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+          <a className="footer-item" href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a className="footer-item" href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
