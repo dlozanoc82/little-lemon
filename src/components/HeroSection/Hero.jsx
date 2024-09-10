c63cd8e57c919923e170c9adacf86230e16df36e
DCODER <dlozanoc862@gmail.com>
2024-09-10 09:16:47 -0500

    feat: add Hero component and its styles
    - Created the Hero component
    - Added CSS styles for the Hero

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -4,7 +4,17 @@ import imageHero from '../../assets/ma_b.jpg'
 const Hero = () => {
   return (
     <div className='hero-background'>
-
+      <div className="hero">
+        <div>
+          <h1 className="title">Little Lemmon</h1>
+          <h2 className="subtitle">Chicago</h2>
+          <p>
+            Nestled in the heart of bustling Chicago, Little Lemon is where modern flair meets cozy nostalgia. Our diverse, artisanal menu, featuring delectable bruschettas, succulent burgers, and refreshing Greek salads, is a testament to our belief that food is an art.
+          </p>
+          <button className="button">Reserve Table</button>
+        </div>
+        <img src={imageHero} alt="Restaurant" className="image" />
+      </div>
     </div>
   )
 }
