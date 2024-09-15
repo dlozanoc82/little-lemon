8ba372a7b3e7c7805aae71d42d1d87f85712e9a5
DCODER <dlozanoc862@gmail.com>
2024-09-15 11:27:19 -0500

    style: fix project-wide styling issues
    - Corrected variables inconsistencies

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         <div>
           <h1 className="title">Little Lemmon</h1>
           <h2 className="subtitle">Chicago</h2>
-          <p>
+          <p className='description-text'>
             Nestled in the heart of bustling Chicago, Little Lemon is where modern flair meets cozy nostalgia. Our diverse, artisanal menu, featuring delectable bruschettas, succulent burgers, and refreshing Greek salads, is a testament to our belief that food is an art.
           </p>
           <Button title={"Reserve a table"} onClick={onClickReserve} />
